f2480e3853c9873c2484c69fea7662ad87976bb7
Jackson Nevins <71613767+MinervaArgus@users.noreply.github.com>
2023-08-21 14:10:31 -0400

    added dependencies, and added the post and get methods for sending and receiving messages

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #[macro_use] extern crate rocket;
-use rocket::tokio::sync::broadcast::{channel};
 
-#[get("/world")]
-fn world() -> &'static str{
-    "Hello, hello"
-}
+use rocket::{State, Shutdown};
+use rocket::fs::{relative, FileServer};
+use rocket::form::Form;
+use rocket::response::stream::{EventStream, Event};
+use rocket::serde::{Serialize, Deserialize};
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::tokio::select; 
+
 
 #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
 #[serde(crate="rocket::serde")]
@@ -17,11 +20,34 @@ struct Message{
     pub message: String,
 }
 
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
+}
+
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+        let _res = queue.send(form.into_inner());
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(state: channel::<Message>(1024).0)
-        .mount("/hello", routes![world])
+        .mount("/", routes![post, events])
 }
 
 
